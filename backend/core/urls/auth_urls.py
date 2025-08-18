6c727bd76c96738e4682f02551244c616f8fcb94
c2ksoria <carlosvsoria1982@gmail.com>
2025-08-18 01:19:25 -0300

    deleted comment

--- backend/core/urls/auth_urls.py
+++ backend/core/urls/auth_urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', RegistroUsuarioAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
-    path('me/', UsuarioActualAPIView.as_view(), name='me'),  # 👈 nuevo endpoint
+    path('me/', UsuarioActualAPIView.as_view(), name='me'),
 ]
