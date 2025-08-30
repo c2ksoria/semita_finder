a025019ba8dc9a976fb49dba399f6d10bf81b19e
c2ksoria <carlosvsoria1982@gmail.com>
2025-08-30 10:20:07 -0300

    style: formatting code with black

--- backend/core/permisions/base.py
+++ backend/core/permisions/base.py
@@ -1,12 +1,15 @@
 from rest_framework.permissions import BasePermission
 
+
 class EsVendedor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.comercios.exists()
 
+
 class EsCliente(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.pedidos.exists()
+
 
 class EsStaff(BasePermission):
     def has_permission(self, request, view):
