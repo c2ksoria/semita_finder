23584131978920fe4a6df74256a265492138e575
c2ksoria <carlosvsoria1982@gmail.com>
2025-08-30 11:44:58 -0300

    style: formatting code with black

--- backend/core/urls/auth_urls.py
+++ backend/core/urls/auth_urls.py
@@ -1,9 +1,14 @@
+from core.views.auth import (
+    LoginAPIView,
+    LogoutAPIView,
+    RegistroUsuarioAPIView,
+    UsuarioActualAPIView,
+)
 from django.urls import path
-from core.views.auth import RegistroUsuarioAPIView, LoginAPIView, LogoutAPIView, UsuarioActualAPIView
 
 urlpatterns = [
-    path('register/', RegistroUsuarioAPIView.as_view(), name='register'),
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout'),
-    path('me/', UsuarioActualAPIView.as_view(), name='me'),
+    path("register/", RegistroUsuarioAPIView.as_view(), name="register"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("me/", UsuarioActualAPIView.as_view(), name="me"),
 ]
