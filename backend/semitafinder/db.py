035fcb80e79be5a3b44f94b94815d5844fdf27a4
c2ksoria <carlosvsoria1982@gmail.com>
2025-08-30 16:10:40 -0300

    style: formatting code with black

--- backend/semitafinder/db.py
+++ backend/semitafinder/db.py
@@ -1,12 +1,12 @@
 import os
 
 POSTGRESSQL = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT_INTERNAL'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT_INTERNAL"),
     }
 }
