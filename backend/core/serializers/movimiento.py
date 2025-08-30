16f1166ce60697906911c6fab5c4036127bba3d6
c2ksoria <carlosvsoria1982@gmail.com>
2025-08-30 10:42:10 -0300

    style: formatting code with black

--- backend/core/serializers/movimiento.py
+++ backend/core/serializers/movimiento.py
@@ -1,9 +1,10 @@
-from rest_framework import serializers
 from core.models import MovimientoPedido
+from rest_framework import serializers
+
 
 class MovimientoPedidoSerializer(serializers.ModelSerializer):
-    usuario_nombre = serializers.CharField(source='usuario.username', read_only=True)
+    usuario_nombre = serializers.CharField(source="usuario.username", read_only=True)
 
     class Meta:
         model = MovimientoPedido
-        fields = ['id', 'estado', 'comentario', 'usuario_nombre', 'fecha_moviminto']
+        fields = ["id", "estado", "comentario", "usuario_nombre", "fecha_moviminto"]
